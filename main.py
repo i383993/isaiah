5261b55d5a5d26225f181c78e0e898749bca005d
impan <impanicker@gmail.com>
2023-12-31 12:26:08 +0530

    i added a new print

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'What is your name superman')
+    print(f'My name is Isaiah')
 
 
 # Press the green button in the gutter to run the script.
